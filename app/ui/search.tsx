d550c3d94e50be7abe15caac8f28e5ff26457711
Lucas <lucasmef@gmail.com>
2025-12-23 16:40:58 -0300

    update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,14 @@
 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
@@ -22,8 +20,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
+
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <div className="relative flex flex-1 shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
@@ -32,7 +31,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
-        }} defaultValue={searchParams.get('query')?.toString()}
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
