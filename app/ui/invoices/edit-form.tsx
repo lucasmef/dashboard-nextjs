dc2c14f3c2ef9d30da3ee1da1a07a6fe81d16914
Lucas Fernandes <51409518+lucasmef@users.noreply.github.com>
2026-04-13 13:47:07 -0300

    Atualizar o edit-form.tsx

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -22,7 +22,8 @@ export default function EditInvoiceForm({
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
-  return <form action={formAction}>
+  return (
+<form action={formAction}>
     <div className="rounded-md bg-gray-50 p-4 md:p-6">
       {/* Customer Name */}
       <div className="mb-4">
@@ -122,5 +123,5 @@ export default function EditInvoiceForm({
       </Link>
       <Button type="submit">Edit Invoice</Button>
     </div>
-  </form>
+  </form>);
 }
